79fc9eddfeeb6974644a54be0c7ae0ca0c5be5df
Guillaume Vetter <guillaume.vetter@epfl.ch>
2019-03-25 16:46:19 +0100

    Constructor done

--- cstring.cpp
+++ cstring.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <cstdio>
 #include "cstring.h"
 
 String::String():content("\0"){}
@@ -7,10 +8,31 @@ String::String(char chars[]):content(chars){}
 
 String::String(String* string):content(string->content){}
 
-String::String(char c):content((char*)c){}
+String::String(char c):content(){
+    char out_string[2];
+    out_string[0] = c;
+    out_string[1] = '\0';
+    this->content = out_string;
 
-String::String(int i):content(i){}
+}
 
-String::String(float f):content(f){}
+String::String(int i){
+    char out_string[20];
+    sprintf(out_string, "%d", i);
+    this->content = out_string;
+}
 
-String::String(bool b){}
+String::String(float f){
+    char out_string[20];
+    sprintf(out_string, "%lf", f);
+    this->content = out_string;
+}
+
+String::String(bool b){
+    if(b){
+        this->content = String("true").content;
+    } else {
+        this->content = String("false").content;
+    }
+
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "cstring.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const char* c = (String("TEST").content);
+    char cha = c[5];
+    const char* vide = String().content;
     return 0;
 }
