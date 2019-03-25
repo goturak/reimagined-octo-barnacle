ad8a0fc2113aa9e9a0d9c4732e23e434fd25862b
Goturak <luca-manu.reisdecarvalho@gmail.com>
2019-03-25 17:29:53 +0100

    bah voila quoi

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 
 int main() {
     char array[] = "One, good, thing, about, music";
+    char array2[] = "One, goos, thing, about, music";
     String* test= new String(array);
-    test->charAt(2)= '4';
-
-
+    String* tes2t= new String(array2);
+    //test->charAt(2)= '4';
+    std::cout<< test->equals(array2)<<std::endl;
+   // printf(test->getContent());
     test->print();
+    tes2t->print();
     return 0;
 }
