8ed0adf8556dc2deadc1024d8a8217108151c2b6
Goturak <luca-manu.reisdecarvalho@gmail.com>
2019-03-25 14:39:55 +0100

    print and length seem ggo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "cstring.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char array[] = "One, good, thing, about, music";
+    String* test= new String(array);
+    test->print();
     return 0;
 }
